654b610fc11261221b33745ef20b8c8880d32787
Taranvirs88 <113557821+Taranvirs88@users.noreply.github.com>
2023-03-23 17:13:11 -0400

    Add project files.

---
+++ Robin da Dhaba/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Robin_da_Dhaba.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
